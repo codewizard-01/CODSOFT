e86523c3690a5bbb1256c000b8c7758b28d0cc0b
Esmatullah Hadi <esmatullahahdi@gmail.com>
2024-11-28 22:38:12 +0430

    Password Generator: Change the design

--- Task_3_Password_Generator/password_generator.py
+++ Task_3_Password_Generator/password_generator.py
@@ -1,54 +1,56 @@
-from random import randint
-import customtkinter as ctk
+import customtkinter
 from tkinter import messagebox
+from random import randint
 
-
-class App(ctk.CTk):
+class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("500x420")
-        self.title("JuiceHackers")
-        self.root_color = self.cget("fg_color")
 
-        # Frames
-        self.password_frame = ctk.CTkFrame(self, fg_color=self.root_color)
-        self.second_frame = ctk.CTkFrame(self, fg_color=self.root_color)
-        self.third_frame = ctk.CTkFrame(self, fg_color=self.root_color)
+        self.title("SuperPassword")
+        self.geometry("700x230")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
-        # Widgets
-        self.title_label = ctk.CTkLabel(self, text="Password Generator",
-                                        font=("Arial", 22, "bold"))
-        self.title_label.pack(pady=10)
+        self.display_frame = customtkinter.CTkFrame(self)
+        self.display_frame.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="new")
+        self.password_frame = customtkinter.CTkFrame(self.display_frame, height=30)
+        self.password_frame.grid(row=2, column=0, columnspan=3, padx=10, pady=(10, 10), sticky="ew")
 
-        # Slider and Entry
-        self.slider_label = ctk.CTkLabel(self.second_frame, text="Password Length", font=("Arial", 14))
-        self.slider_label.grid(row=0, column=0, padx=5, pady=5)
+        self.label = customtkinter.CTkLabel(self.display_frame, text="Generate a password",
+                                                   font=("Arial", 14, "bold"))
+        self.label.grid(row=0, column=0, padx=10, pady=10)
 
-        self.slider = ctk.CTkSlider(self.second_frame, from_=0, to=100, command=self.update_number)
+        self.slider_label = customtkinter.CTkLabel(self.display_frame, text="Characters", font=("Arial", 14))
+        self.slider_label.grid(row=1, column=0, padx=10, pady=(0, 10), sticky="w")
+
+        self.slider = customtkinter.CTkSlider(self.display_frame, from_=0, to=100, command=self.update_number)
         self.slider.set(0)
-        self.slider.grid(row=0, column=1, padx=10, pady=5)
+        self.slider.grid(row=1, column=1, padx=10, pady=5)
 
-        self.number_entry = ctk.CTkEntry(self.second_frame, width=50, font=("Arial", 14))
-        self.number_entry.grid(row=0, column=2, padx=10, pady=5)
+        self.number_entry = customtkinter.CTkEntry(self.display_frame, width=50, font=("Arial", 14))
+        self.number_entry.grid(row=1, column=2, padx=10, pady=5)
 
-        # Button
-        self.generate_button = ctk.CTkButton(self.third_frame, text="Generate Password", command=self.generate_password,
-                                             font=("Arial", 16), height=36)
-        self.generate_button.pack(pady=15)
+        # Checkbox frame with its content
+        self.checkbox_frame = customtkinter.CTkFrame(self)
+        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+        self.checkbox_1 = customtkinter.CTkCheckBox(self.checkbox_frame, text="Letters")
+        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_2 = customtkinter.CTkCheckBox(self.checkbox_frame, text="Numbers")
+        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_3 = customtkinter.CTkCheckBox(self.checkbox_frame, text="signs")
+        self.checkbox_3.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
 
-        self.message = ctk.CTkLabel(self, text="Made with ❤ by Esmat.", font=("sans-serif", 9))
-        self.message.pack(side="bottom")
-        # Pack the Frames
-        self.second_frame.pack(pady=10)
-        self.third_frame.pack(pady=10)
-        self.password_frame.pack()
+        # Generate password button
+        self.button = customtkinter.CTkButton(self.display_frame, text="Generate Password",
+                                              command=self.generate_password)
+        self.button.grid(row=3, column=0, columnspan=3, padx=10, pady=10, sticky="ew")
 
     def show_error(self):
         messagebox.showerror("Error", "Please choose the length of your password.")
 
     def update_number(self, value):
         # Update the entry with the value of the slider
-        self.number_entry.delete(0, ctk.END)
+        self.number_entry.delete(0, customtkinter.END)
         self.number_entry.insert(0, str(int(float(value))))
 
     def generate_password(self):
@@ -60,13 +62,14 @@ class App(ctk.CTk):
                 generated_password += chr(randint(33, 127))
 
             # Display the Generated Password
-            display_password = ctk.CTkLabel(self.password_frame, text=generated_password,
+            for widget in self.password_frame.winfo_children():
+                widget.destroy()
+            display_password = customtkinter.CTkLabel(self.password_frame, text=generated_password,
                                             font=("Arial", 14))
             display_password.pack(pady=10)
         else:
             self.show_error()
 
 
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+app = App()
+app.mainloop()
